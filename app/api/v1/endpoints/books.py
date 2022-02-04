6b841f6458cbb3cff76ab4998493e70782f11ad2
nobaluis <noba_luis@hotmail.com>
2022-02-04 13:06:52 -0600

    Added CRUD operations to books endpoit

--- app/api/v1/endpoints/books.py
+++ app/api/v1/endpoints/books.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 
+from ..model.book import Book
 from ..service import books_service as books
 
 router = APIRouter()
@@ -10,6 +11,16 @@ async def get_books():
     return [{"title": "hello"}, {"title:": "world"}]
 
 
+@router.post("/")
+async def post_book(book: Book):
+    books.create_book(book)
+
+
+@router.put("/")
+async def put_book(book: Book):
+    return books.update_book(book)
+
+
 @router.get("/by-author/{author}")
 async def get_books_by_author(author: str):
     return books.get_books_by_author(author)
@@ -18,3 +29,8 @@ async def get_books_by_author(author: str):
 @router.get("/by-author/{author}/by-title/{title}")
 async def get_book(author: str, title: str):
     return books.get_book(title, author)
+
+
+@router.delete("/by-author/{author}/by-title/{title}")
+async def delete_book(author: str, title: str):
+    books.delete_book(title, author)
--- app/api/v1/service/books_service.py
+++ app/api/v1/service/books_service.py
@@ -1,9 +1,9 @@
 from typing import List
 
+from boto3.dynamodb.conditions import Key
+
 from .db import session
 from ..model.book import Book
-
-from boto3.dynamodb.conditions import Key
 
 table = session.Table('Books')
 
@@ -17,3 +17,34 @@ def get_books_by_author(author: str) -> List[Book]:
     query_key = Key('Author').eq(author)
     result = table.query(KeyConditionExpression=query_key)
     return [Book.parse_obj(item) for item in result['Items']]
+
+
+def create_book(book: Book) -> None:
+    table.put_item(Item=book.dict(by_alias=True))
+
+
+def create_books(books: List[Book]) -> None:
+    with table.batch_writer() as batch:
+        for book in books:
+            batch.put_item(Item=book.dict())
+
+
+def update_book(book: Book) -> Book:
+    result = table.update_item(
+        Key={"Author": book.author, "Title": book.title},
+        ExpressionAttributeNames={
+            "#formats": "Formats",
+            "#category": "Category"
+        },
+        ExpressionAttributeValues={
+            ":new_for": book.formats,
+            ":new_cat": book.category
+        },
+        UpdateExpression="SET #formats = :new_for, #category = :new_cat",
+        ReturnValues="UPDATED_NEW"
+    )
+    return Book.parse_obj(result['Attributes'])
+
+
+def delete_book(title: str, author: str) -> None:
+    table.delete_item(Key={"Author": author, "Title": title})
